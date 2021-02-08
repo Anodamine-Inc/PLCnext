c42e881770295a04d3d2f6e5f5354ad2d59d2289
Jared Hattingh <jaredhattingh@Jareds-iMac.local>
2021-02-08 11:18:44 -0600

    updated to use hmac keys

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const cron = require('node-cron');
 const axios = require('axios');
 const https = require('https');
+const crypto = require('crypto');
 
-const PLC_URL = process.env.PLC_URL || '<plc-url-here>';
-const WEBHOOK_URL = process.env.WEBHOOK_URL || '<webhook-url-here>';
+
+const PLC_URL = process.env.PLC_URL || '<plc-url>';
+const WEBHOOK_URL = process.env.WEBHOOK_URL || '<api-url>';
 // Defaults to every minute
 const CRON_SHEDULE = process.env.CRON_SHEDULE || '* * * * *';
 
+const HMAC_KEY = process.env.HMAC_KEY;
+const API_KEY = process.env.API_KEY;
+const ID = process.env.ID;
 
 function getPlcData() {
-    let data;
     const agent = new https.Agent({  
         rejectUnauthorized: false
       });
-    axios.get(PLC_URL, { httpsAgent: agent}).then(res => {
-        data = res.data;
+    return axios.get(PLC_URL, { httpsAgent: agent}).then(res => {
+        return res.data;
     })
     .catch(err => {
         console.log(err);
     });
-    return data;
-}
+
+};
 
 function notifyWebhook(payload) {
-    axios.post(WEBHOOK_URL, payload).then(res => {
+    payload.id = ID;
+    let dataString = JSON.stringify(payload);
+    let hmac = crypto.createHmac('sha1', HMAC_KEY)
+      .update(dataString)
+      .digest('hex');
+      
+    axios.post(WEBHOOK_URL, payload, { params: { key: API_KEY }, headers: { 'Content-Type':'application/json', 'x-hmac': hmac } }).then(res => {
+        console.log(res);
         console.log('Notified webhook')
     })
     .catch(err => {
@@ -34,9 +45,6 @@ function notifyWebhook(payload) {
 console.log('App has started... waiting for cron. - Duane Bester');
 
 cron.schedule(CRON_SHEDULE, () => {
-  console.log('Getting PLC Data...');
-  let plcData = getPlcData();
-  if(plcData) {
-    notifyWebhook(plcData);
-  }
-});
+    console.log('Getting PLC Data...');
+    getPlcData().then(data => notifyWebhook(data));
+  });
